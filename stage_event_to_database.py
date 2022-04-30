bfc1a57368ce8c9dd69d7ca7ed1dc9d66921fc16
Leo <46682213+leooel97895750@users.noreply.github.com>
2022-04-30 17:17:04 +0800

    Update stage_event_to_database.py

--- stage_event_to_database.py
+++ stage_event_to_database.py
@@ -16,7 +16,7 @@ mydb = mysql.connector.connect(host=passwd['host'], user=passwd['user'], passwor
 mycursor = mydb.cursor()
 
 stage_path = r"G:/共用雲端硬碟/Sleep center data/auto_detection/sleep_scoring_AI/2022_Sleep_Scoring_AI/2022result/result_answer/"
-event_path = r"G:/共用雲端硬碟/Sleep center data/auto_detection/respiratory_detect/workshop0606data/result/"
+event_path = r"G:/共用雲端硬碟/Sleep center data/auto_detection/respiratory_detect/2022data/result/"
 # 讀取預測與偵測結果的檔案
 # stage
 psg_file_id = input("psg file id ? : ")
@@ -43,8 +43,8 @@ start = input("press enter key to conitune...")
 
 
 # 標記此檔案有預測資料
-sql = "update psg_file (prediction)"
-val = (1)
+sql = "update psg_file set prediction = %s where id = %s"
+val = (1, psg_file_id)
 mycursor.execute(sql, val)
 mydb.commit()
 
@@ -54,10 +54,11 @@ for i in range(epoch):
     stage = data_stage[i].split('\n')[0].split(',')
     
     # 插入資料庫
-    sql = "insert into stage_prediction (psg_file_id, epoch, golden_stage, node_stage, auto_stage, stage_smooth, stage_smooth_reliable)"
+    sql = "insert into stage_prediction (psg_file_id, epoch, golden_stage, node_stage, auto_stage, stage_smooth, stage_smooth_reliable) values(%s, %s, %s, %s, %s, %s, %s)"
     val = (psg_file_id, i, stage[0], stage[1], stage[2], stage[3], stage[4])
     mycursor.execute(sql, val)
     mydb.commit()
+
     
     
 # 插入事件
@@ -67,14 +68,15 @@ timefrom = 0
 timeto = 0
 start = 0
 for i in range(len(apnea)):
-    if apnea[i] == 1 and start == 0:
+    if apnea[i] == '1' and start == 0:
+        print('start')
         timefrom = i
         start = 1
-    elif apnea[i] == 0 and start == 1:
+    elif apnea[i] == '0' and start == 1:
         timeto = i - 1
         start = 0
         # 將此段事件插入資料庫
-        sql = "insert into event_prediction (psg_file_id, type, timefrom, timeto, eventid, channelid)"
+        sql = "insert into event_prediction (psg_file_id, type, timefrom, timeto, eventid, channelid) values(%s, %s, %s, %s, %s, %s)"
         val = (psg_file_id, "Obstructive Apnea", timefrom, timeto, eventid, 12)
         mycursor.execute(sql, val)
         mydb.commit()
@@ -82,14 +84,14 @@ for i in range(len(apnea)):
 
 start = 0
 for i in range(len(hypopnea)):
-    if hypopnea[i] == 1 and start == 0:
+    if hypopnea[i] == '1' and start == 0:
         timefrom = i
         start = 1
-    elif hypopnea[i] == 0 and start == 1:
+    elif hypopnea[i] == '0' and start == 1:
         timeto = i - 1
         start = 0
         # 將此段事件插入資料庫
-        sql = "insert into event_prediction (psg_file_id, type, timefrom, timeto, eventid, channelid)"
+        sql = "insert into event_prediction (psg_file_id, type, timefrom, timeto, eventid, channelid) values(%s, %s, %s, %s, %s, %s)"
         val = (psg_file_id, "Obstructive Hypopnea", timefrom, timeto, eventid, 12)
         mycursor.execute(sql, val)
         mydb.commit()
@@ -97,14 +99,14 @@ for i in range(len(hypopnea)):
         
 start = 0
 for i in range(len(spo2_artifact)):
-    if spo2_artifact[i] == 1 and start == 0:
+    if spo2_artifact[i] == '1' and start == 0:
         timefrom = i
         start = 1
-    elif spo2_artifact[i] == 0 and start == 1:
+    elif spo2_artifact[i] == '0' and start == 1:
         timeto = i - 1
         start = 0
         # 將此段事件插入資料庫
-        sql = "insert into event_prediction (psg_file_id, type, timefrom, timeto, eventid, channelid)"
+        sql = "insert into event_prediction (psg_file_id, type, timefrom, timeto, eventid, channelid) values(%s, %s, %s, %s, %s, %s)"
         val = (psg_file_id, "SpO2 Artifact", timefrom, timeto, eventid, 16)
         mycursor.execute(sql, val)
         mydb.commit()
@@ -112,14 +114,14 @@ for i in range(len(spo2_artifact)):
         
 start = 0
 for i in range(len(spo2)):
-    if spo2[i] == 1 and start == 0:
+    if spo2[i] == '1' and start == 0:
         timefrom = i
         start = 1
-    elif spo2[i] == 0 and start == 1:
+    elif spo2[i] == '0' and start == 1:
         timeto = i - 1
         start = 0
         # 將此段事件插入資料庫
-        sql = "insert into event_prediction (psg_file_id, type, timefrom, timeto, eventid, channelid)"
+        sql = "insert into event_prediction (psg_file_id, type, timefrom, timeto, eventid, channelid) values(%s, %s, %s, %s, %s, %s)"
         val = (psg_file_id, "SpO2 Desat", timefrom, timeto, eventid, 16)
         mycursor.execute(sql, val)
         mydb.commit()
@@ -127,15 +129,16 @@ for i in range(len(spo2)):
 
 start = 0
 for i in range(len(arousal)):
-    if arousal[i] == 1 and start == 0:
+    if arousal[i] == '1' and start == 0:
         timefrom = i
         start = 1
-    elif arousal[i] == 0 and start == 1:
+    elif arousal[i] == '0' and start == 1:
         timeto = i - 1
         start = 0
         # 將此段事件插入資料庫
-        sql = "insert into event_prediction (psg_file_id, type, timefrom, timeto, eventid, channelid)"
+        sql = "insert into event_prediction (psg_file_id, type, timefrom, timeto, eventid, channelid) values(%s, %s, %s, %s, %s, %s)"
         val = (psg_file_id, "ARO SPONT", timefrom, timeto, eventid, 2)
         mycursor.execute(sql, val)
         mydb.commit()
         eventid = eventid + 1
+        
